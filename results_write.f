e4dd1750a04e83f85630bf2b746a87eb5ecd9073
Dave Gaskell <gaskelld@jlab.org>
2026-07-30 07:04:12 -0400

    Make output variables in tree/ntuple the same for exclusive and SIDIS processes

--- results_write.f
+++ results_write.f
@@ -6,7 +6,7 @@
 	include 'hbook.inc'
 	include 'simulate.inc'
 
-	integer i
+	integer i,m
 	real*8	ntu(80)
 	type(event_main):: main
 	type(event):: vertex, orig, recon
@@ -14,8 +14,6 @@
 !local (e,e'p) calculations:
 	real*8 poftheta		!p as calculated from theta, assuming elastic.
 	real*8 corrsing		!'corrected singles' for elastic
-!	real*8 mm,mm2		!missing mass (assuming struck nucleon)
-!	real*8 mmA,mmA2		!missing mass (assuming struck nucleus)
 	real*8 Pm_Heepx,Pm_Heepy,Pm_Heepz	!Pm components for Heepcheck.
 
 !local (e,e'pi/K) calculations:
@@ -30,27 +28,7 @@
 ! add option to write event even if it failed when doing_phsp.
 
 	if (.not.success) return
-
-*	if (phot1.gt.lim1) write(6,*) 'phot1,lim1=',phot1,lim1
-*	if (phot2.gt.lim2) then
-*	   write(6,*) 'phot2,lim2=',phot2,lim2
-*	   write(6,*) egamma_tot_max-egamma_used(1)
-*	   write(6,*) vertex.e.E - edge.e.E.min
-*	endif
-*	if (phot3.gt.lim3) write(6,*) 'phot3,lim3=',phot3,lim3
-
-
-!Calculate some proton/pion/kaon specific stuff:
-!
-!	if (doing_pion .or. doing_kaon) then
-!	  mm2 = recon.Em**2 - recon.Pm**2
-!	  mm  = sqrt(abs(mm2)) * abs(mm2)/mm2
-!	  mmA2= (recon.nu + targ.M - recon.p.E)**2 - recon.Pm**2
-!	  mmA = sqrt(abs(mmA2)) * abs(mmA2)/mmA2
-!	  t = recon.Q2 - Mh2
-!     >	    + 2*(recon.nu*recon.p.E - recon.p.P*recon.q*cos(recon.theta_pq))
-!	endif
-
+	
 	if (doing_hyd_elast .or. doing_deuterium .or. doing_heavy) then
 	  poftheta = Mp*Ebeam / (2*ebeam*sin(recon%e%theta/2.)**2 + Mp)
 	  corrsing = recon%e%P - poftheta
@@ -61,206 +39,285 @@
 	  Pm_Heepx =  -recon%Pmx
 	endif
 
+	m=0
 	if(electron_arm.eq.1 .or. electron_arm.eq.3.or. electron_arm.eq.7)then !electron = right side.
-	  ntu(1) = recon%e%delta
-	  ntu(2) = recon%e%yptar			!mr
-	  ntu(3) = recon%e%xptar			!mr
-	  ntu(4) = recon%e%z
-	  ntu(5) = main%FP%e%x
-	  ntu(6) = main%FP%e%dx				!mr
-	  ntu(7) = main%FP%e%y
-	  ntu(8) = main%FP%e%dy				!mr
-	  ntu(9) = orig%e%delta
-	  ntu(10) = orig%e%yptar			!mr
-	  ntu(11) = orig%e%xptar			!mr
-	  ntu(12) = main%target%z*spec%e%sin_th
-	  ntu(13) = recon%p%delta
-	  ntu(14) = recon%p%yptar			!mr
-	  ntu(15) = recon%p%xptar			!mr
- 	  ntu(16) = recon%p%z
- 	  ntu(17) = main%FP%p%x
-	  ntu(18) = main%FP%p%dx			!mr
- 	  ntu(19) = main%FP%p%y
- 	  ntu(20) = main%FP%p%dy			!mr
-	  ntu(21) = orig%p%delta
-	  ntu(22) = orig%p%yptar			!mr
-	  ntu(23) = orig%p%xptar			!mr
- 	  ntu(24) = -main%target%z*spec%p%sin_th
+	  m=m+1 
+	  ntu(m) = recon%e%delta ! 1
+	  m=m+1
+	  ntu(m) = recon%e%yptar ! 2  mr
+	  m=m+1
+	  ntu(m) = recon%e%xptar ! 3  mr
+	  m=m+1
+	  ntu(m) = recon%e%z	! 4
+	  m=m+1
+	  ntu(m) = main%FP%e%x ! 5
+	  m=m+1
+	  ntu(m) = main%FP%e%dx	! 6 mr
+	  m=m+1
+	  ntu(m) = main%FP%e%y ! 7 
+	  m=m+1
+	  ntu(m) = main%FP%e%dy	!8 mr
+	  m=m+1
+	  ntu(m) = orig%e%delta ! 9 
+	  m=m+1
+	  ntu(m) = orig%e%yptar	! 10 mr
+	  m=m+1
+	  ntu(m) = orig%e%xptar	! 11 mr
+	  m=m+1
+	  ntu(m) = main%target%z*spec%e%sin_th ! 12
+	  m=m+1
+	  ntu(m) = recon%p%delta ! 13
+	  m=m+1
+	  ntu(m) = recon%p%yptar ! 14 mr
+	  m=m+1
+	  ntu(m) = recon%p%xptar  ! 15 mr
+	  m=m+1
+ 	  ntu(m) = recon%p%z	! 16
+	  m=m+1
+ 	  ntu(m) = main%FP%p%x	! 17
+	  m=m+1
+	  ntu(m) = main%FP%p%dx	! 18 mr
+	  m=m+1
+ 	  ntu(m) = main%FP%p%y	! 19
+	  m=m+1
+ 	  ntu(m) = main%FP%p%dy	! 20 mr
+	  m=m+1
+	  ntu(m) = orig%p%delta ! 21
+	  m=m+1
+	  ntu(m) = orig%p%yptar	! 22 mr
+	  m=m+1
+	  ntu(m) = orig%p%xptar	! 23 mr
+	  m=m+1
+ 	  ntu(m) = -main%target%z*spec%p%sin_th
 	else if (electron_arm.eq.2 .or. electron_arm.eq.4 .or.
      >		 electron_arm.eq.5 .or. electron_arm.eq.6.or. electron_arm.eq.8) then  !e- = left.
-	  ntu(1) = recon%p%delta
-	  ntu(2) = recon%p%yptar			!mr
-	  ntu(3) = recon%p%xptar			!mr
-	  ntu(4) = recon%p%z
-	  ntu(5) = main%FP%p%x
-	  ntu(6) = main%FP%p%dx				!mr
-	  ntu(7) = main%FP%p%y
-	  ntu(8) = main%FP%p%dy				!mr
-c	  ntu(9) = vertex%p%delta
-c	  ntu(10) = vertex%p%yptar			!mr
-c	  ntu(11) = vertex%p%xptar			!mr
-	  ntu(9) = orig%p%delta
-	  ntu(10) = orig%p%yptar			!mr
-	  ntu(11) = orig%p%xptar			!mr
-	  ntu(12) = main%target%z*spec%p%sin_th
-	  ntu(13) = recon%e%delta
-	  ntu(14) = recon%e%yptar			!mr
-	  ntu(15) = recon%e%xptar			!mr
-	  ntu(16) = recon%e%z
- 	  ntu(17) = main%FP%e%x
-	  ntu(18) = main%FP%e%dx			!mr
- 	  ntu(19) = main%FP%e%y
- 	  ntu(20) = main%FP%e%dy			!mr
-	  ntu(21) = orig%e%delta
-	  ntu(22) = orig%e%yptar			!mr
-	  ntu(23) = orig%e%xptar			!mr
-	  ntu(24) = -main%target%z*spec%e%sin_th
+	  m=m+1
+	  ntu(m) = recon%p%delta ! 1
+	  m=m+1
+	  ntu(m) = recon%p%yptar ! 2 mr
+	  m=m+1
+	  ntu(m) = recon%p%xptar !3 mr
+	  m=m+1
+	  ntu(m) = recon%p%z	! 4 
+	  m=m+1
+	  ntu(m) = main%FP%p%x	! 5
+	  m=m+1
+	  ntu(m) = main%FP%p%dx	! 6 mr
+	  m=m+1
+	  ntu(m) = main%FP%p%y ! 7
+	  m=m+1
+	  ntu(m) = main%FP%p%dy	! 8 mr
+	  m=m+1
+	  ntu(m) = orig%p%delta ! 9 
+	  m=m+1
+	  ntu(m) = orig%p%yptar	! 10 mr
+	  m=m+1
+	  ntu(m) = orig%p%xptar	! 11 mr
+	  m=m+1
+	  ntu(m) = main%target%z*spec%p%sin_th ! 12
+	  m=m+1
+	  ntu(m) = recon%e%delta ! 13
+	  m=m+1
+	  ntu(m) = recon%e%yptar ! 14 mr
+	  m=m+1
+	  ntu(m) = recon%e%xptar ! 15 mr
+	  m=m+1
+	  ntu(m) = recon%e%z ! 16
+	  m=m+1
+ 	  ntu(m) = main%FP%e%x ! 17
+	  m=m+1
+	  ntu(m) = main%FP%e%dx	! 18 mr
+	  m=m+1
+ 	  ntu(m) = main%FP%e%y ! 19
+	  m=m+1
+ 	  ntu(m) = main%FP%e%dy	! 20 mr
+	  m=m+1
+	  ntu(m) = orig%e%delta ! 21
+	  m=m+1
+	  ntu(m) = orig%e%yptar	! 22 mr
+	  m=m+1
+	  ntu(m) = orig%e%xptar	! 23 mr
+	  m=m+1
+	  ntu(m) = -main%target%z*spec%e%sin_th ! 24
 	else
 	  write (6,*) 'results_write not yet set up for your spectrometers.'
+	  stop
 	endif
-	ntu(25) = recon%q/1000.				!q - GeV/c	
-	ntu(26) = recon%nu/1000.			!nu - GeV
-	ntu(27) = recon%Q2/1.e6				!Q^2 - (GeV/c)^2
-	ntu(28) = recon%W/1000.				!W - GeV/c
-	ntu(29) = recon%epsilon				!epsilon
-	ntu(30) = recon%Em/1000.			!GeV
-	ntu(31) = recon%Pm/1000.			!GeV/c
-	ntu(32) = recon%theta_pq			!theta_pq - radians
-	ntu(33) = recon%phi_pq				!phi_pq - radians
-
-	if (doing_pion .or. doing_kaon .or. doing_delta) then
-	  ntu(34) = ntup%mm/1000.			!missmass (nucleon)
-	  ntu(35) = ntup%mmA/1000.			!missmass (nucleus)
-	  ntu(36) = recon%p%P/1000.			!ppi - GeV/c
-	  ntu(37) = ntup%t/1.e6				!t - GeV^2
-	  ntu(38) = recon%PmPar/1000.
-	  ntu(39) = recon%PmPer/1000.
-	  ntu(40) = recon%PmOop/1000.
-	  ntu(41) = -main%target%rastery		!fry - cm
-	  ntu(42) = ntup%radphot/1000.			!radphot - GeV
+	m=m+1
+	ntu(m) = recon%q/1000.	! 25 q - GeV/c
+	m=m+1
+	ntu(m) = recon%nu/1000.	! 26 nu - GeV
+	m=m+1
+	ntu(m) = recon%Q2/1.e6	! 27 Q^2 - (GeV/c)^2
+	m=m+1
+	ntu(m) = recon%W/1000.	! 28 W - GeV/c
+	m=m+1
+	ntu(m) = recon%xbj	! 29 xbj
+	m=m+1
+	ntu(m) = recon%epsilon	! 30 epsilon
+	m=m+1
+	ntu(m) = recon%Em/1000.	! 31 GeV
+	m=m+1
+	ntu(m) = recon%Pm/1000.	! 32 GeV/c
+	m=m+1
+	ntu(m) = vertex%q/1000.	! 33 q - GeV/c
+	m=m+1
+	ntu(m) = vertex%nu/1000. ! 34 nu - GeV
+	m=m+1
+	ntu(m) = vertex%Q2/1.e6	! 35 Q^2 - (GeV/c)^2
+	m=m+1
+	ntu(m) = main%W/1000.	! 36 W - GeV/c
+	m=m+1
+	ntu(m) = vertex%xbj	! 37 xbj
+	m=m+1
+	ntu(m) = main%epsilon	! 38 epsilon
+	m=m+1
+	ntu(m) = vertex%Emiss/1000. ! 39 GeV
+	m=m+1
+	ntu(m) = vertex%Pmiss/1000. ! 40 GeV/c
+	if (doing_pion .or. doing_kaon .or. doing_delta .or. doing_semi .or. doing_rho) then
+	  m=m+1
+	  ntu(m) = ntup%mm/1000. ! 41 missmass (nucleon)
+	  m=m+1
+	  ntu(m) = ntup%mmA/1000. ! 42 missmass (nucleus)
+	  m=m+1
+	  ntu(m) = recon%p%P/1000. ! 43 ppi - GeV/c
+	  m=m+1
+	  ntu(m) = ntup%t/1.e6	! 44 t - GeV^2
+	  m=m+1
+	  ntu(m) = main%t/1.e6	! 45
+	  m=m+1
+	  ntu(m) = recon%zhad	! 46 z
+	  m=m+1
+ 	  ntu(m) = vertex%zhad	! 47
+	  m=m+1
+	  ntu(m) = recon%pt2/1.e06 ! 48 pt**2 -GeV^2
+	  m=m+1
+	  ntu(m) = vertex%pt2/1.e06 ! 49
+	  m=m+1
+	  ntu(m) = recon%theta_pq ! 50 theta_pq - radians
+	  m=m+1
+	  ntu(m) = recon%phi_pq	! 51 phi_pq - radians
+	  m=m+1
+	  ntu(m) = main%theta_pq ! 52 theta_pq - radians
+	  m=m+1
+	  ntu(m) = main%phi_pq	! 53 phi_pq - radians
+	  m=m+1
+	  ntu(m) = recon%PmPar/1000. ! 54
+	  m=m+1
+	  ntu(m) = recon%PmPer/1000. ! 55
+	  m=m+1
+	  ntu(m) = recon%PmOop/1000. ! 56
+	  m=m+1
+	  ntu(m) = -main%target%rastery	!57 fry - cm
+	  m=m+1
+	  ntu(m) = ntup%radphot/1000. ! 58 radphot - GeV
 	  dummy = pferx*vertex%uq%x + pfery*vertex%uq%y + pferz*vertex%uq%z
 	  if (dummy.eq.0) dummy=1.e-20
-	  ntu(43) = pfer/1000.*abs(dummy)/dummy		!p_fermi - GeV/c
-	  ntu(44) = main%sigcc				!d5sig
-	  ntu(45) = ntup%sigcm				!pion sig_cm
-	  ntu(46) = main%weight
-	  ntu(47) = decdist				!decay distance (cm)
-	  ntu(48) = sqrt(Mh2_final)
-	  ntu(49) = pfer/1000.*dummy			!p_fermi along q.
-	  ntu(50) = vertex%Q2/1.e6
-	  ntu(51) = main%w/1.e3
-	  ntu(52) = main%t/1.e6
-	  ntu(53) = main%phi_pq
-	  if(using_tgt_field) then
-	     ntu(54) = recon%theta_tarq
-	     ntu(55) = recon%phi_targ
-	     ntu(56) = recon%beta
-	     ntu(57) = recon%phi_s
-	     ntu(58) = recon%phi_c
-	     ntu(59) = main%beta
-	     ntu(60) = vertex%phi_s
-	     ntu(61) = vertex%phi_c	     
-	     if (doing_kaon) then
-		ntu(62) = ntup%sigcm1 !sigcm - saghai model
-		ntu(63) = ntup%sigcm2 !sigcm - factorized.
-	     endif
-	  else
-	     if (doing_kaon) then
-		ntu(54) = ntup%sigcm1 !sigcm - saghai model
-		ntu(55) = ntup%sigcm2 !sigcm - factorized.
-	     endif
+	  m=m+1
+	  ntu(m) = pfer/1000.*abs(dummy)/dummy ! 59 p_fermi - GeV/c
+	  m=m+1
+	  ntu(m) = main%sigcc	! 60 d5sig or d6sig
+	  m=m+1
+	  ntu(m) = ntup%sigcm	! 61 pion sig_cm, or SIDIS sig_had
+ 	  if (doing_kaon) then
+	     m=m+1	  
+	     ntu(m) = ntup%sigcm1 ! 61 sigcm - saghai model
+	     m=m+1	  
+	     ntu(m) = ntup%sigcm2 ! 61.5 sigcm - factorized.
+	  endif
+	  m=m+1		  
+	  ntu(m) = main%weight ! 62
+	  m=m+1
+	  ntu(m) = decdist	! 63 decay distance (cm)
+	  m=m+1		
+	  ntu(m) = sqrt(Mh2_final) ! 64
+	  if(doing_rho) then
+	     m=m+1
+	     ntu(m) = ntup%rhomass ! 65
+	     m=m+1
+	     ntu(m) = ntup%rhotheta ! 66
+	     m=m+1
+	     ntu(m) = ntup%mmA/1000 !67
 	  endif
 	  if(doing_pizero) then
-	     ntu(54) = ntup%xcal_gamma1
-	     ntu(55) = ntup%ycal_gamma1
-	     ntu(56) = ntup%gamma1(1)
-	     ntu(57) = ntup%gamma1(2)
-	     ntu(58) = ntup%gamma1(3)
-	     ntu(59) = ntup%gamma1(4)
-	     ntu(60) = ntup%xcal_gamma2
-	     ntu(61) = ntup%ycal_gamma2
-	     ntu(62) = ntup%gamma2(1)
-	     ntu(63) = ntup%gamma2(2)
-	     ntu(64) = ntup%gamma2(3)
-	     ntu(65) = ntup%gamma2(4)
+	     m=m+1
+	     ntu(m) = ntup%xcal_gamma1 ! 68
+	     m=m+1
+	     ntu(m) = ntup%ycal_gamma1 ! 69
+	     m=m+1
+	     ntu(m) = ntup%gamma1(1) ! 70
+	     m=m+1
+	     ntu(m) = ntup%gamma1(2) ! 71
+	     m=m+1
+	     ntu(m) = ntup%gamma1(3) ! 72
+	     m=m+1
+	     ntu(m) = ntup%gamma1(4) ! 73
+	     m=m+1
+	     ntu(m) = ntup%xcal_gamma2 ! 74
+	     m=m+1
+	     ntu(m) = ntup%ycal_gamma2 ! 75
+	     m=m+1
+	     ntu(m) = ntup%gamma2(1) ! 76
+	     m=m+1
+	     ntu(m) = ntup%gamma2(2) ! 77 
+	     m=m+1
+	     ntu(m) = ntup%gamma2(3) ! 78
+	     m=m+1
+	     ntu(m) = ntup%gamma2(4) ! 79
 	  endif
-	else if (doing_semi.or.doing_rho) then
-	  ntu(34) = ntup%mm/1000.			!missmass (nucleon)
-	  ntu(35) = recon%p%P/1000.			!ppi - GeV/c
-	  ntu(36) = ntup%t/1.e6				!t - GeV^2
-	  ntu(37) = -main%target%rastery		!fry - cm
-	  ntu(38) = ntup%radphot/1000.			!radphot - GeV
-	  ntu(39) = main%sigcc				!d5sig
-	  ntu(40) = main%sigcent			!d5sig - central kin.
-	  ntu(41) = main%weight
-	  ntu(42) = decdist				!decay distance (cm)
-	  ntu(43) = sqrt(Mh2_final)
-	  ntu(44) = recon%zhad
- 	  ntu(45) = vertex%zhad
-	  ntu(46) = recon%pt2/1.e06
-	  ntu(47) = vertex%pt2/1.e06
-	  ntu(48) = recon%xbj
-	  ntu(49) = vertex%xbj
-	  ntu(50) = acos(vertex%uq%z)
-	  ntu(51) = ntup%sigcm
-	  ntu(52) = main%davejac
-	  ntu(53) = main%johnjac
-	  dummy = pferx*vertex%uq%x + pfery*vertex%uq%y + pferz*vertex%uq%z
-	  ntu(54) = pfer/1000.*abs(dummy)/dummy		!p_fermi - GeV/c
-	  ntu(55) = ntup%xfermi
-	  ntu(56) = main%phi_pq
 	  if(using_tgt_field) then
-	     ntu(57) = recon%theta_tarq
-	     ntu(58) = recon%phi_targ
-	     ntu(59) = recon%beta
-	     ntu(60) = recon%phi_s
-	     ntu(61) = recon%phi_c
-	     ntu(62) = main%beta
-	     ntu(63) = vertex%phi_s
-	     ntu(64) = vertex%phi_c	     
-	     if(doing_rho) then
-		ntu(65) = ntup%rhomass
-		ntu(66) = ntup%rhotheta
-	     endif
-	  else
-	     if(doing_rho) then
-		ntu(57) = ntup%rhomass
-		ntu(58) = ntup%rhotheta
-		ntu(59) = ntup%mmA/1000.			!missmass (nucleus)HSV
-	     endif
+	     m=m+1
+	     ntu(m) = recon%theta_tarq ! 80
+	     m=m+1
+	     ntu(m) = recon%phi_targ ! 81
+	     m=m+1
+	     ntu(m) = recon%beta ! 82
+	     m=m+1
+	     ntu(m) = recon%phi_s ! 83
+	     m=m+1
+	     ntu(m) = recon%phi_c ! 84
+	     m=m+1
+	     ntu(m) = main%beta	! 85
+	     m=m+1
+	     ntu(m) = vertex%phi_s ! 86
+	     m=m+1
+	     ntu(m) = vertex%phi_c ! 87	     
 	  endif
-	  if(doing_pizero) then
-	     ntu(57) = ntup%xcal_gamma1
-	     ntu(58) = ntup%ycal_gamma1
-	     ntu(59) = ntup%gamma1(1)
-	     ntu(60) = ntup%gamma1(2)
-	     ntu(61) = ntup%gamma1(3)
-	     ntu(62) = ntup%gamma1(4)
-	     ntu(63) = ntup%xcal_gamma2
-	     ntu(64) = ntup%ycal_gamma2
-	     ntu(65) = ntup%gamma2(1)
-	     ntu(66) = ntup%gamma2(2)
-	     ntu(67) = ntup%gamma2(3)
-	     ntu(68) = ntup%gamma2(4)
-	  endif
-	else if (doing_hyd_elast .or. doing_deuterium .or. doing_heavy .or. doing_nuc_elast) then
-	  ntu(34) = corrsing/1000.
-	  ntu(35) = Pm_Heepx/1000.
-	  ntu(36) = Pm_Heepy/1000.
-	  ntu(37) = Pm_Heepz/1000.
-	  ntu(38) = recon%PmPar/1000.
-	  ntu(39) = recon%PmPer/1000.
-	  ntu(40) = recon%PmOop/1000.
-	  ntu(41) = -main%target%rastery		!fry - cm
-	  ntu(42) = ntup%radphot/1000.			!radphot - GeV
-	  ntu(43) = main%sigcc
-	  ntu(44) = main%weight
-	  ntu(45) = recon%e%theta
-	  ntu(46) = recon%p%theta
-	  
+	else if (doing_hyd_elast .or. doing_deuterium .or. doing_heavy) then
+	   m=m+1
+	   ntu(m) = corrsing/1000. ! 41
+	   m=m+1
+	   ntu(m) = Pm_Heepx/1000. ! 42
+	   m=m+1
+	   ntu(m) = Pm_Heepy/1000. ! 43
+	   m=m+1
+	   ntu(m) = Pm_Heepz/1000. ! 44
+	   m=m+1
+	   ntu(m) = recon%PmPar/1000. ! 45
+	   m=m+1
+	   ntu(m) = recon%PmPer/1000. ! 46
+	   m=m+1
+	   ntu(m) = recon%PmOop/1000. ! 47 
+	   m=m+1
+	   ntu(m) = -main%target%rastery ! 48 fry - cm
+	   m=m+1
+	   ntu(m) = ntup%radphot/1000. ! 49 radphot - GeV
+	   m=m+1
+	   ntu(m) = main%sigcc ! 50
+	   m=m+1
+	   ntu(m) = main%weight ! 51
+	   m=m+1
+	   ntu(m) = recon%e%theta ! 52
+	   m=m+1
+	   ntu(m) = recon%p%theta ! 53
 	endif
 
 c	call HFN(NtupleID,ntu)
+	if(m.ne.NtupleSize) then
+	   write(6,*) 'number of output quantities not the same as output array size'
+	   stop
+	endif
 	do i=1,NtupleSize
 	   write(NtupleIO) ntu(i)
 	enddo
